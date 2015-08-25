5cd6e667a5b1fc9d08c76fcb1061f81e7c65c736
Lanfei <CooLanfei@163.com>
2015-08-25 16:58:11 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# grunt-filerev-usemin
+
+> Replace references to grunt-filerev files.
+
+## Getting Started
+This plugin requires Grunt `~0.4.5`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-filerev-usemin --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-filerev-usemin');
+```
+
+## The "filerev_usemin" task
+
+### Overview
+In your project's Gruntfile, add a section named `filerev_usemin` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  filerev: {
+    images: {
+      src: 'images/**.*'
+    }
+  },
+  filerev_usemin: {
+    dist: {
+      src: '*.html'
+    },
+    activities: {
+      options: {
+        root: 'activities'
+      },
+      dist: {
+        src: 'activities/*.html'
+      }
+    }
+  },
+});
+```
+
+### Options
+
+#### options.root
+Type: `String`
+
+The root path of page files, defaults to empty.
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
---
+++ tasks/filerev_usemin.js
@@ -0,0 +1,76 @@
+/*
+ * grunt-filerev-usemin
+ * https://github.com/Lanfei/grunt-filerev-usemin
+ *
+ * Copyright (c) 2015 Lanfei
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var path = require('path');
+
+var STYLE_FILE_RE = /(css|sass|less)$/;
+var ASSETS_RE = /(['"(])([^'":# \(\)\?]+\.[^'":# \(\)\?]+)(['")?#])/g;
+
+module.exports = function (grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('filerev_usemin', 'Replace references to grunt-rev files.', function () {
+    if (!grunt.filerev || !grunt.filerev.summary) {
+      throw new Error('Grunt task `filerev` is required.');
+      return;
+    }
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      root: '.'
+    });
+    var root = options.root;
+    var summary = grunt.filerev.summary;
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function (file) {
+      // Concat specified files.
+      file.src.forEach(function (filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        }
+
+        grunt.log.writeln('✔ '.green + filepath);
+
+        var isStyleFile = STYLE_FILE_RE.test(filepath);
+        var fileDir = path.dirname(filepath);
+        var source = grunt.file.read(filepath);
+        source = source.replace(ASSETS_RE, function ($, $1, $2, $3) {
+          var referenceDir;
+          var relativePath;
+
+          if (isStyleFile) {
+            referenceDir = fileDir;
+          } else {
+            referenceDir = root;
+          }
+          relativePath = path.join(referenceDir, $2);
+
+          var result = summary[relativePath];
+          if (result) {
+            result = path.relative(referenceDir, result);
+            grunt.log.writeln('  ' + $2 + ' ==> '.grey + result);
+
+            return $1 + result + $3;
+          } else {
+            return $;
+          }
+        });
+
+        grunt.file.write(filepath, source);
+      });
+    });
+  });
+
+};
